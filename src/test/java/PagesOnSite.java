392025a868e5af160e442502f64d9784e3e8fddc
Weisz Dávid <yaraclen@gmail.com>
2022-06-08 09:24:17 +0200

    pages on site method with switch

--- src/test/java/PagesOnSite.java
+++ src/test/java/PagesOnSite.java
@@ -30,16 +30,37 @@ public class PagesOnSite extends StartDriver {
 
     public void checkresult() {
         //Meg kéne keresni az oldal első sorát, és aszerint ellenőrizni a hivatkozásokat. Így szét tudnám szedni a click és a checkresult metódust
-        if (driver.getPageSource().contains("Math Typesetting")) {
-            assertEquals(expectedUrlPageMath, driver.getCurrentUrl());
-        } else if (driver.getPageSource().contains("Placeholder Text")) {
-            assertEquals(expectedUrlPlaceholder, driver.getCurrentUrl());
-        } else if (driver.getPageSource().contains("Rich Content")) {
-            assertEquals(expectedUrlRichContent, driver.getCurrentUrl());
-        } else if (driver.getPageSource().contains("Markdown")) {
-            assertEquals(expectedUrlMarkdown, driver.getCurrentUrl());
-        } else if (driver.getPageSource().contains("Image Test")) {
-            assertEquals(expectedUrlImage, driver.getCurrentUrl());
+        String searchText = "";
+        switch (searchText) {
+            case "Math Typesetting":
+                driver.getPageSource().contains("Math Typesetting");
+                assertEquals(expectedUrlPageMath, driver.getCurrentUrl());
+            case "Placeholder Text":
+                driver.getPageSource().contains("Placeholder Text");
+                assertEquals(expectedUrlPlaceholder, driver.getCurrentUrl());
+            case "Rich Content":
+                driver.getPageSource().contains("Rich Content");
+                assertEquals(expectedUrlRichContent, driver.getCurrentUrl());
+            case "Markdown":
+                driver.getPageSource().contains("Markdown");
+                assertEquals(expectedUrlMarkdown, driver.getCurrentUrl());
+            case "Image Test":
+                driver.getPageSource().contains("Image Test");
+                assertEquals(expectedUrlImage, driver.getCurrentUrl());
         }
+        /*
+        if (driver.getPageSource().contains("Image Test")) {
+            assertEquals(expectedUrlImage, driver.getCurrentUrl());
+        } if (driver.getPageSource().contains("Markdown")) {
+            assertEquals(expectedUrlMarkdown, driver.getCurrentUrl());
+        } if (driver.getPageSource().contains("Rich Content")) {
+            assertEquals(expectedUrlRichContent, driver.getCurrentUrl());
+        } if (driver.getPageSource().contains("Placeholder Text")) {
+            assertEquals(expectedUrlPlaceholder, driver.getCurrentUrl());
+        } if (driver.getPageSource().contains("Math Typesetting")) {
+            assertEquals(expectedUrlPageMath, driver.getCurrentUrl());
+        }
+        */
+
     }
 }
